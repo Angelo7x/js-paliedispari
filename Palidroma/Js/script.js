fd7ddd80e0216c00ccb8f6f9676f3f42e2f42b51
Angelo7x <abennici96@gmail.com>
2021-08-31 16:35:03 +0200

    creati file

---
+++ Palidroma/Js/script.js
@@ -0,0 +1,7 @@
+// Chiedere all'utente una parola 
+var parolaUtente = prompt("Inserisci una parola");
+// Creare una funzione per capire se la parola inserita è palindroma
+function palindroma (parola) {
+    for ( var i = parolaUtente.length ; i <= 0; i--)
+        if (parolaUtente == )
+}
