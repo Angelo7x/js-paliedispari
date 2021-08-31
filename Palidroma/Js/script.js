089b90b3b56932daee1dc5b741a4841732106238
Angelo7x <abennici96@gmail.com>
2021-08-31 17:02:09 +0200

    up

--- Palidroma/Js/script.js
+++ Palidroma/Js/script.js
@@ -1,7 +1,12 @@
 // Chiedere all'utente una parola 
 var parolaUtente = prompt("Inserisci una parola");
 // Creare una funzione per capire se la parola inserita è palindroma
-function palindroma (parola) {
-    for ( var i = parolaUtente.length ; i <= 0; i--)
-        if (parolaUtente == )
+function palindroma() {
+    for ( var i = parolaUtente.length ; i <= 0; i--) {
+
+    return inverse;
+    }
 }
+
+var inverse = palindroma();
+console.log(inverse);
